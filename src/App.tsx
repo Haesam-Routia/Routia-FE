1ef81cefb6e4d0042895378d2c1be71fe1b6f2bb
chanju <chanju030303@naver.com>
2026-08-13 16:19:24 +0900

    first setting commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <div className="rounded-2xl bg-white p-8 shadow-xl">
+        <h1 className="text-4xl font-bold text-blue-500">Tailwind 연결 성공</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
